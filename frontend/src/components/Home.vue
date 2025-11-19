e8a50ebef8cb4b0457366ea04142e3b9a5709c18
Milan Navratil <navratil.milann@gmail.com>
2025-11-19 12:41:10 +0100

    [EDIT] Frontend - Form

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form action="#" method="POST" class="space-y-6">
+        <div>
+          <div class="mt-2">
+            <input
+              placeholder="UUID"
+              class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+            />
+          </div>
+        </div>
+
+        <div>
+          <div class="mt-2">
+            <input
+              type="text"
+              placeholder="Battery percent"
+              class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+            />
+          </div>
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            class="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+          >
+            Send
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+<style>
+html,
+body,
+#app {
+  height: 100%;
+}
+</style>
